67a8b602077d3fde6e85e5709fc07f7f9b5225c9
Ludovic Giraud <ludovic.giraud@essec.edu>
2021-03-30 13:48:13 +0200

    Date coordinate still not added properly

--- concatenate_all_files_in_dir.py
+++ concatenate_all_files_in_dir.py
@@ -1,9 +1,15 @@
 import time, glob
 import shutil
+import xarray as xr
+import datetime
 
 def without_shutil_text(outfilename):
     with open(outfilename, 'wb') as outfile:
         for filename in glob.glob('data/pm25/*.nc'):
+            datefromfile = datetime.datetime.strptime(filename.replace("pm25-","").replace(".nc","").replace('data/pm25/',""), '%Y-%m-%d')
+            pm25 = xr.open_dataset(filename)
+            pm25 = pm25.assign_coords(date= datefromfile)
+            pm25.to_netcdf(filename)
             if filename == outfilename:
                 # don't want to copy the output into the output
                 continue
